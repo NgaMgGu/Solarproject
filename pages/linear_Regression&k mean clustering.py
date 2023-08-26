36bca79660142e3b3a8e0479bc3cee2373f4e5ad
Sophia66756 <121043894+Sophia66756@users.noreply.github.com>
2023-08-26 16:29:08 +0630

    Update linear_Regression&k mean clustering.py

--- pages/linear_Regression&k mean clustering.py
+++ pages/linear_Regression&k mean clustering.py
@@ -1,7 +1,3 @@
-import matplotlib
-matplotlib.use('Agg')  # Use Agg backend
-import matplotlib.pyplot as plt
-
 from sklearn.cluster import KMeans
 import streamlit as st
 import pandas as pd
@@ -51,151 +47,173 @@ from sklearn.metrics import r2_score, mean_squared_error
 import matplotlib.pyplot as plt
 import streamlit as st
 
+
+lgmodel= st.sidebar.checkbox('Linear Regression')
+kmmodel = st.sidebar.checkbox('Kmeans')
 # Load the DataFrame from the provided data
 # ... (Your previous code for loading data and extracting x_values and y_values)
+if lgmodel:
+    # Reshape x_values to a 2D array
+    x = np.array(x_values).reshape(-1, 1)
 
-# Reshape x_values to a 2D array
-x = np.array(x_values).reshape(-1, 1)
+    # Create a LinearRegression model and fit it
+    model = LinearRegression()
+    model.fit(x, y)
 
-# Create a LinearRegression model and fit it
-model = LinearRegression()
-model.fit(x, y)
+    # Predict LCOE value for the year 2023
+    year_2023 = np.array([2023]).reshape(-1, 1)  # Reshape for prediction
+    predicted_lcoe_2023 = model.predict(year_2023)
 
-# Predict LCOE value for the year 2023
-year_2023 = np.array([2023]).reshape(-1, 1)  # Reshape for prediction
-predicted_lcoe_2023 = model.predict(year_2023)
+    # Predict y values using the model
+    predicted_y = model.predict(x)
 
-# Predict y values using the model
-predicted_y = model.predict(x)
+    # Calculate R-squared
+    r2 = r2_score(y, predicted_y)
 
-# Calculate R-squared
-r2 = r2_score(y, predicted_y)
+    # Calculate mean squared error
+    mse = mean_squared_error(y, predicted_y)
 
-# Calculate mean squared error
-mse = mean_squared_error(y, predicted_y)
+    # Create the linear equation string
+    linear_equation = f'LCOE = {model.intercept_:.4f} + {model.coef_[0]:.4f} * Year'
 
-# Create the linear equation string
-linear_equation = f'LCOE = {model.intercept_:.4f} + {model.coef_[0]:.4f} * Year'
+    # Create the plot
+    plt.figure(figsize=(10, 6))
 
-# Create the plot
-plt.figure(figsize=(10, 6))
+    # Plot the data points
+    plt.scatter(x, y, color='blue', label='Actual Data')
 
-# Plot the data points
-plt.scatter(x, y, color='blue', label='Actual Data')
+    # Plot the linear regression line
+    plt.plot(x, predicted_y, color='red', label='Linear Regression Line')
 
-# Plot the linear regression line
-plt.plot(x, predicted_y, color='red', label='Linear Regression Line')
+    # Plot the predicted 2023 value
+    plt.scatter(year_2023, predicted_lcoe_2023, color='green', label='Predicted 2023')
 
-# Plot the predicted 2023 value
-plt.scatter(year_2023, predicted_lcoe_2023, color='green', label='Predicted 2023')
+    # Adding labels and title
+    plt.xlabel('Years')
+    plt.ylabel('Weighted average Levelized Cost of Electricity values (USD/kWh)')
+    plt.title('Linear Regression of Weighted average Levelized Cost of Electricity (LCOE) values')
 
-# Adding labels and title
-plt.xlabel('Years')
-plt.ylabel('Weighted average Levelized Cost of Electricity values (USD/kWh)')
-plt.title('Linear Regression of Weighted average Levelized Cost of Electricity (LCOE) values')
+    # Adding legend and grid
+    plt.legend()
+    plt.grid(True)
 
-# Adding legend and grid
-plt.legend()
-plt.grid(True)
+    # Adding annotations with adjusted x-coordinates
+    plt.text(2012, 0.4, linear_equation, fontsize=12, color='black')
+    plt.text(2012, 0.35, f'R-squared: {r2:.4f}', fontsize=12, color='black')
+    plt.text(2012, 0.3, f'MSE: {mse:.4f}', fontsize=12, color='black')
 
-# Adding annotations with adjusted x-coordinates
-plt.text(2012, 0.4, linear_equation, fontsize=12, color='black')
-plt.text(2012, 0.35, f'R-squared: {r2:.4f}', fontsize=12, color='black')
-plt.text(2012, 0.3, f'MSE: {mse:.4f}', fontsize=12, color='black')
+    # Display the LCOE data plot using Streamlit
+    st.pyplot(plt.gcf())
 
-# Display the LCOE data plot using Streamlit
-st.pyplot(plt.gcf())
+if kmmodel:
 
+    df = pd.read_csv('GHI_yangon_v3.csv')
+    dataindex= df.iloc[:, [11]]
+    print(dataindex)
 
+    st.title('Grouping of Solar Energy Genereation Potential wih Cost Using k-mean')
+    #where Yi is centroid for observation Xi.
+    #The main goal is to maximize number of clusters and in limiting case each data point becomes its own cluster centroid.
+    #Compute K-Means clustering for different values of K by varying K from 1 to 10 clusters.
+    #For each K, calculate the total within-cluster sum of square (WCSS).
+    #Plot the curve of WCSS vs the number of clusters K.
+    #The location of a bend (knee) in the plot is generally considered as an indicator of the appropriate number of clusters.
+    st.header('Chose number of cluster using elbow Method')
+    wcss = []
+    for k in range(1,11):
+        kmeans = KMeans(n_clusters=k, init="k-means++")
+        kmeans.fit(df.iloc[:,11:])
+        wcss.append(kmeans.inertia_)
+    plt.figure(figsize=(12,6))
+    plt.grid()
+    plt.plot(range(1,11),wcss, linewidth=2, color="red", marker ="8")
+    plt.xlabel("K Value")
+    plt.xticks(np.arange(1,11,1))
+    plt.ylabel("WCSS")
+    st.pyplot(plt.gcf())
 
-df = pd.read_csv('GHI_yangon_v3.csv')
-dataindex= df.iloc[:, [11]]
-print(dataindex)
+    # Features for 3D plot
+    features_3d = ["Shape_Area(sqm)", "GHI_MW", "Cost_USD(M)"]
 
+    # Create the 3D scatter plot
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection='3d')
 
-
-
-
-
-
-
-st.title('Grouping of Solar Energy Genereation Potential wih Cost Using k-mean')
-#where Yi is centroid for observation Xi.
-#The main goal is to maximize number of clusters and in limiting case each data point becomes its own cluster centroid.
-#Compute K-Means clustering for different values of K by varying K from 1 to 10 clusters.
-#For each K, calculate the total within-cluster sum of square (WCSS).
-#Plot the curve of WCSS vs the number of clusters K.
-#The location of a bend (knee) in the plot is generally considered as an indicator of the appropriate number of clusters.
-st.header('Evaluating number of cluster using elbow method')
-wcss = []
-for k in range(1,11):
-    kmeans = KMeans(n_clusters=k, init="k-means++")
-    kmeans.fit(df.iloc[:,11:])
-    wcss.append(kmeans.inertia_)
-plt.figure(figsize=(12,6))
-plt.grid()
-plt.plot(range(1,11),wcss, linewidth=2, color="red", marker ="8")
-plt.xlabel("K Value")
-plt.xticks(np.arange(1,11,1))
-plt.ylabel("WCSS")
-st.pyplot(plt.gcf())
-
-# Features for 3D plot
-features_3d = ["Shape_Area(sqm)", "GHI_MW", "Cost_USD(M)"]
-
-# Create the 3D scatter plot
-fig = plt.figure(figsize=(10, 8))
-ax = fig.add_subplot(111, projection='3d')
-
-# KMeans clustering
-km = KMeans(n_clusters=3)
-clusters = km.fit(df[features_3d])
-clusters1 = clusters.predict(df[features_3d])
-df["label"] = clusters1
+    # KMeans clustering
+    km = KMeans(n_clusters=3)
+    clusters = km.fit(df[features_3d])
+    clusters1 = clusters.predict(df[features_3d])
+    df["label"] = clusters1
 
 
 
 
 
-# Print selected columns and cluster labels
+    # Print selected columns and cluster labels
 
 
-st.header('3D Plot data points with different colors for each cluster')
+    st.header('3D Plot data points with different colors for each cluster')
 
 
-# Plot data points in 3D with different colors for each cluster
-for label in df["label"].unique():
-   
-    ax.scatter(df.loc[df["label"] == label, features_3d[0]],
-               df.loc[df["label"] == label, features_3d[1]],
-               df.loc[df["label"] == label, features_3d[2]],
-               label=f'Cluster {label}')
+    # Plot data points in 3D with different colors for each cluster
+    for label in df["label"].unique():
+       
+        ax.scatter(df.loc[df["label"] == label, features_3d[0]],
+                   df.loc[df["label"] == label, features_3d[1]],
+                   df.loc[df["label"] == label, features_3d[2]],
+                   label=f'Cluster {label}')
 
-ax.set_xlabel(features_3d[0])
-ax.set_ylabel(features_3d[1])
-ax.set_zlabel(features_3d[2])
-ax.legend()
+    ax.set_xlabel(features_3d[0])
+    ax.set_ylabel(features_3d[1])
+    ax.set_zlabel(features_3d[2])
+    ax.legend()
 
-
-
-# Show the plot in Streamlit
-
-st.pyplot(fig)
+    st.pyplot(fig)
 
 
-st.title('New dataframe with cost and label cluster')
-st.dataframe(df)
-st.markdown("##")
+    st.title('New Dataframw with Cost')
+    st.dataframe(df)
+    st.markdown("##")
 
-table_data = df[features_3d + ["label"]]
-print(table_data)
-df1=(table_data.groupby('label').mean())
-st.title('Mean value of three clusters in terms of Area, MW and Cost (M)')
-st.dataframe(df1)
-st.markdown("##")
+    table_data = df[features_3d + ["label"]]
+    print(table_data)
+    df1=(table_data.groupby('label').mean())
+    st.title('Mean value of three clusters in terms of Area, MW and Cost (M)')
+    st.dataframe(df1)
+    st.markdown("##")
+    
+    with st.sidebar.form("my_form1"):
+        
+        st.title('Cluster the new circustances')
+
+        area = st.number_input('Shape_Area(sqm)')
+        ghimW = st.number_input('GHI_MW')
+        Cost = st.number_input('Cost_USD(M)')
+        
+        st.markdown("##")
+        
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            #st.write("slider", slider_val, "checkbox", checkbox_val)
+            data = {'Shape_Area(sqm)': area,
+                        'GHI_MW': ghimW,
+                        'Cost_USD(M)': Cost}
+            features = pd.DataFrame(data, index=[0])
+            #features['Shape_Area(sqm)'] = features['Shape_Area(sqm)'].astype(str).astype(float)
+            #features['GHI_MW'] = features['GHI_MW'].astype(str).astype(float)
+            #features['Cost_USD(M)'] = features['Cost_USD(M)'].astype(str).astype(float)
+            #features = features.astype({'Shape_Area(sqm)':'float', 'GHI_MW':float, 'Cost_USD':float})
+            st.write(area)
+            clusters2 = clusters.predict(features)
+            if (clusters2==0):
+                st.write('The Solar generaton category is category 1')
+            elif (clusters2==1):
+                st.write('The Solar generaton category is category 2')
+            else:
+                st.write('The Solar generaton category is category 3')
 
 
-
+    st.markdown("##")
 
 
 
